2beef12c565d3bf57e7e5c59aea84b72e8b28bce
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-06-12 20:10:22 +0530

    minor

--- src/main/java/com/healthsync/config/SchedulingConfiguration.java
+++ src/main/java/com/healthsync/config/SchedulingConfiguration.java
@@ -1,10 +1,13 @@
 package com.healthsync.config;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -17,6 +20,10 @@ import com.healthsync.repository.ReminderRepository;
 public class SchedulingConfiguration {
 	@Autowired
     private ReminderRepository reminderRepository;
+	
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+
     
     @Scheduled(fixedDelay = 60000) // Runs every minute
     public void checkReminders() {
@@ -34,6 +41,12 @@ public class SchedulingConfiguration {
     }
     
     private void sendNotification(String email, String message) {
-        // Implementation for sending notification
+    	// Prepare the notification payload
+        Map<String, String> notification = new HashMap<>();
+        notification.put("email", email);
+        notification.put("message", message);
+
+        // Send the notification to the frontend using WebSockets
+        messagingTemplate.convertAndSend("/topic/notifications", notification);
     }
 }
